fa3bf60fc15549886b3361a36f9aea5a4ef9075a
MalronWall <thibaut.tourte17@gmail.com>
2018-04-15 17:36:24 +0200

    PROBLEM => encodage

--- src/Manager/PostManager.php
+++ src/Manager/PostManager.php
@@ -16,9 +16,28 @@ class PostManager extends AbstractManager
     public function getPosts()
     {
         $results = [];
-        $req = $this->db->requestDb('SELECT * FROM post');
-        $req->fetchAll();
-        return $req;
+/*
+        try {
+            $bdd = new \PDO(
+                "mysql:hostname=localhost;dbname=oc_back_p5",
+                'root',
+                ''
+            );
+            $bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $bdd->setAttribute(\PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8'");
+        } catch (\PDOException $e) {
+            die("Erreur : " . $e->getMessage());
+        }
+
+        $req = $bdd->prepare("SELECT * FROM post ORDER BY id");
+
+        $req -> execute();
+*/
+        $req = $this->db->requestDb('SELECT * FROM post ORDER BY id');
+
+        $results = $req->fetchAll();
+
+        return $results;
         /*
         foreach ($req->fetchAll() as $datas) {
             $results[] = Hydrator::hydrate(Post::class, serialize(array_values($datas)));
